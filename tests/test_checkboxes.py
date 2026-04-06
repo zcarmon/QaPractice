31b85859448f21b2d11b9244d1da0f17460cf271
Zvi Carmon <zcarmon@gmail.com>
2026-04-06 11:44:04 +0300

    complete the check boxes test

--- tests/test_checkboxes.py
+++ tests/test_checkboxes.py
@@ -5,13 +5,46 @@ BASE_URL = "https://www.qa-practice.com/elements/checkbox/"
 SINGLE = "single_checkbox"
 MULTI = "mult_checkbox"
 
-def test_single(page :Page):
+def test_single_unchecked(page :Page):
     page.goto(BASE_URL+SINGLE)
     box_locator = page.locator('[class="form-check-label"]')
-    if(box_locator.text_content() == "Select me or not"):
-        box_locator.uncheck()
-        check_result_by_click(page, FIELD_ID_SUBMIT, FIELD_ID_RESULT_TEXT, FIELD_RESULT_EMPTY)
+    box_locator.uncheck()
+    page.locator(FIELD_ID_SUBMIT).click()
+    expect(page.locator('[id="content"]')).not_to_have_id("result")
 
-    check_result_by_click(page,FIELD_ID_SUBMIT,FIELD_ID_RESULT_TEXT,FIELD_SUBMITTED)
-    pass
+def test_single_checked(page: Page):
+    page.goto(BASE_URL + SINGLE)
+    box_locator = page.locator('[class="form-check-label"]')
+    box_locator.check()
+    page.locator(FIELD_ID_SUBMIT).click()
+    expect(page.locator(FIELD_ID_RESULT_TEXT)).to_have_text("Select me or not", ignore_case=True)
 
+def test_multi_unchecked(page: Page):
+    page.goto(BASE_URL + MULTI)
+    box_locator = page.locator('[class="form-check form-check-inline"]').get_by_role("checkbox")
+    i = 0
+    while i < box_locator.count(): #checkbox in box_locator:
+        box_locator.nth(i).uncheck()
+        i = i + 1
+
+    page.locator(FIELD_ID_SUBMIT).click()
+    expect(page.locator('[id="content"]')).not_to_have_id("result")
+
+def test_multi_checked(page: Page):
+    #access the url
+    page.goto(BASE_URL + MULTI)
+    #find the checkboxes
+    box_locator = page.locator('[class="form-check form-check-inline"]').get_by_role("checkbox")
+    i = 0
+    res_text = ""
+    #loop over all the checkboxes
+    while i < box_locator.count():  # checkbox in box_locator:
+        box_locator.nth(i).check()
+        #the result is the concatenation of the checkbox's inner text
+        res_text = res_text + page.locator('label[for="id_checkboxes_' + str(i) + '"]').inner_text().strip().lower() + ", "
+        i = i + 1
+
+    #click the button
+    page.locator(FIELD_ID_SUBMIT).click()
+    #check the expected result
+    expect(page.locator('[id="content"]')).not_to_have_id("result")
